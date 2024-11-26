c03bf12fd06710ce52caff33924ea392844a495c
BigDaveCoding <bigdavecoding@gmail.com>
2024-11-26 18:36:01 +0000

    working on animations
    trying out different animations on the hero text and picture

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
+    gsap.registerPlugin(ScrollTrigger,TextPlugin);
 
     let intro_animation_complete = false;
 
@@ -10,13 +11,38 @@ document.addEventListener('DOMContentLoaded', function() {
             onComplete: function() {
                 intro_animation_complete = true;
                 console.log("intro animation complete is:" + intro_animation_complete)
+
+                triggerHomepage();
             }
         });
-        tl.from("#kankei_logo", { duration: 2.5, opacity: 0 })
-            .to("#kankei_logo", { duration: 1.5, scale: 0.25 })
-            .to(".logo_container", {duration: 1.7, height: "15vh", ease: "none"}, "<");
+        tl.from("#kankei_logo", { duration: 1, opacity: 0 })
+            .to("#kankei_logo", { duration: 1, scale: 0.25 })
+            .to(".logo_container", {duration: 1, height: "10vh", ease: "none"}, "<");
             
         console.log("intro animation complete is: " + intro_animation_complete)
+    }
+
+    function triggerHomepage() {
+        var hero_content = document.querySelector(".hero_text_picture");
+        hero_content.style.display = "flex";
+
+        gsap.to(".hero_text h1", {
+            duration: 2,
+            text: "WE ARE KANKEI",
+            ease: "power2.inOut",
+            stagger: 0.5,
+            oncComplete: function() {
+                console.log("WE ARE KANKEI Text animation complete");
+            }
+        });
+        gsap.from(".hero_picture", {duration: 5, opacity: 0});
+        gsap.to(".hero_text p", {
+            duration: 2,
+            text: "CLIMB THE LADDER OF SUCCESS WITH US",
+            ease: "power2.inOut",
+            delay: 1
+        }
+        )
     }
 
     
